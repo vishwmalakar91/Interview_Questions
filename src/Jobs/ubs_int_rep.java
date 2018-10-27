d566ff1448a2d394c70796c595079d36f857326e
vishwanath Malakar <vishwanathmalakar@gmail.com>
2018-10-27 23:21:53 +0530

    adding ubs int

---
+++ src/Jobs/ubs_int_rep.java
@@ -0,0 +1,33 @@
+package Jobs;
+
+//Java program to find first non-repeating 
+//element. 
+
+public class ubs_int_rep {
+
+	static int firstNonRepeating(int arr[], int n) 
+	{ 
+		for (int i = 0; i < n; i++) { 
+			int j; 
+			for (j = 0; j < n; j++) 
+				if (i != j && arr[i] == arr[j]) 
+					break; 
+			if (j == n) 
+				return arr[i]; 
+		} 
+		
+		return -1; 
+	} 
+	
+	//Driver code 
+	public static void main (String[] args) 
+	{ 
+		
+		int arr[] = { 9, 4, 9, 6, 7, 4 }; 
+		int n = arr.length; 
+		
+		System.out.print(firstNonRepeating(arr, n)); 
+	} 
+} 
+
+//This code is contributed by Anant Agarwal. 
---
+++ src/Jobs/ubs_int_stars.java
@@ -0,0 +1,42 @@
+package Jobs;
+class ubs_int_start
+{
+	public static void main(String[] args)
+	{
+		final int n = 5;
+
+		// Print upper half
+		for (int i = 1; i <= n; i++)
+		{
+			// Print space
+			for (int j = i; j < n; j++) {
+				System.out.print(' ');
+			}
+
+			// Print '*'
+			for (int k = 1; k < 2*i; k++) {
+				System.out.print('*');
+			}
+
+			// Move to the next line
+			System.out.print(System.lineSeparator());
+		}
+
+		// Print lower half
+		for (int i = n - 1; i >= 1; i--)
+		{
+			// Print space
+			for (int j = n; j > i; j--) {
+				System.out.print(' ');
+			}
+
+			// Print '*'
+			for (int k = 1; k < (i * 2); k++) {
+				System.out.print('*');
+			}
+
+			// Move to the next line
+			System.out.print(System.lineSeparator());
+		}
+	}
+}
