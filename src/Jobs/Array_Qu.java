e70041e8bc5cb2e8693f7c8d49934a9bde109cb3
vishwanath Malakar <vishwanathmalakar@gmail.com>
2018-10-29 01:20:31 +0530

    adding new qu

--- src/Jobs/Array_Qu.java
+++ src/Jobs/Array_Qu.java
@@ -6,10 +6,41 @@ public class Array_Qu {
 		
 		String[] names = new String[10];
 		names[1]= "vishwa";
-		System.out.println(names[1]);
+		//System.out.println(names[1]);
 		
 		int length_of_array=names.length;
-		System.out.println(length_of_array);
+		//System.out.println(length_of_array);
+		Array_Qu obj =new Array_Qu();
+		obj.test();
+		
+	}
+	
+	
+	public void test() {
+		String Names[]= {"Vishwa","Neha","Pranab","Deepak","Umesh","Neha","Vishwa","Bhavana","Bhavana"};
+		int i=0,j=0;
+		boolean b=false;
+		for(i=0;i<Names.length;i++)
+		{
+			for(j=i;j<(Names.length-1);j++)
+			{
+			if(Names[i].equals(Names[j+1]))
+			{
+				System.out.println("repeat is : "+Names[i]);
+				b=false;
+			}
+			else
+			{
+				b=true;
+			}
+			
+			}
+			if(b==true)
+			{
+				System.out.println("Unique is : "+Names[i]);
+			}
+		}
+
 	}
 }
 
